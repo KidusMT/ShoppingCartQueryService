adfb3fb6039c6e0319e0a07db1667e585abf9f93
Kidus Mamuye Tekeste <kidusmamuye@gmail.com>
2021-08-11 05:52:02 -0500

    added ELK stack logging

--- src/main/java/project/swa/ShoppingCartQueryService/controller/ShoppingCartController.java
+++ src/main/java/project/swa/ShoppingCartQueryService/controller/ShoppingCartController.java
@@ -1,9 +1,13 @@
 package project.swa.ShoppingCartQueryService.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import project.swa.ShoppingCartQueryService.exception.ShoppingCartNotFoundException;
 import project.swa.ShoppingCartQueryService.service.ShoppingCartDTO;
 import project.swa.ShoppingCartQueryService.service.ShoppingCartService;
@@ -13,12 +17,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/shoppingquery")
 public class ShoppingCartController {
+    private static final Logger logger = LoggerFactory.getLogger(ShoppingCartController.class.getName());
 
     @Autowired
     private ShoppingCartService shoppingCartService;
 
     @GetMapping
     public ResponseEntity<?> getCustomers() {
+        logger.info("Calling GET /shoppingquery");
         List<ShoppingCartDTO> customerDTO1 = shoppingCartService.getAll();
         try {
             if (customerDTO1 != null) {
